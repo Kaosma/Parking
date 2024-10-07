f10e2f25b209214f90459eb1bb1f012608575b08
Kaosma <erre360@hotmail.com>
2024-10-08 00:59:02 +0200

    feat(input flow): add a more thourough flow to the user input

--- main.dart
+++ main.dart
@@ -6,25 +6,28 @@ void choiceHandler(String? choice) {
       personMainHandler();
       break;
     case '2':
-      personMainHandler();
+      vehicleMainHandler();
       break;
     case '3':
-      personMainHandler();
+      parkingSpaceMainHandler();
       break;
     case '4':
-      personMainHandler();
+      parkingMainHandler();
       break;
     case '5':
-      print('Du har valt att avsluta, hej då!');
       return;
     default:
-      print('Ogiltigt val. Välj gärna mellan alternativ 1-5');
+      stdout.write('''
+
+        'Ogiltigt val. Välj gärna mellan alternativ 1-5'
+      ''');
   }
 }
 
 void personMainHandler() {
   stdout.write('''
-    Du har valt att hantera Personer. Vad vill du göra?
+
+    Du har valt att hantera personer. Vad vill du göra?
     1. Skapa ny person
     2. Visa alla personer
     3. Uppdatera person
@@ -56,7 +59,142 @@ void personMainHandler() {
         validatePersonInput();
         return;
       default:
-        print('Ogiltigt val. Välj gärna mellan alternativ 1-5');
+        stdout.write('''
+
+          'Ogiltigt val. Välj gärna mellan alternativ 1-5'
+        ''');
+        break;
+    }
+  }
+}
+
+void vehicleMainHandler() {
+  stdout.write('''
+
+    Du har valt att hantera fordon. Vad vill du göra?
+    1. Lägg till nytt fordon
+    2. Visa alla fordon
+    3. Uppdatera fordon
+    4. Ta bort fordon
+    5. Gå tillbaka till huvudmenyn
+
+    Välj ett alternativ (1-5):
+  ''');
+  while (!validVehicleInput) {
+    final input = stdin.readLineSync();
+    switch (input) {
+      case '1':
+        validateVehicleInput();
+        print('nu får du skapa ett fordon!');
+        break;
+      case '2':
+        validateVehicleInput();
+        print('nu visas alla fordon!');
+        break;
+      case '3':
+        validateVehicleInput();
+        print('nu får du uppdatera ett fordon!');
+        break;
+      case '4':
+        validateVehicleInput();
+        print('nu får du ta bort ett fordon!');
+        break;
+      case '5':
+        validateVehicleInput();
+        return;
+      default:
+        stdout.write('''
+
+          'Ogiltigt val. Välj gärna mellan alternativ 1-5'
+        ''');
+        break;
+    }
+  }
+}
+
+void parkingSpaceMainHandler() {
+  stdout.write('''
+
+    Du har valt att hantera parkeringsplatser. Vad vill du göra?
+    1. Lägg till ny parkeringsplats
+    2. Visa alla parkeringsplatser
+    3. Uppdatera parkeringsplats
+    4. Ta bort parkeringsplats
+    5. Gå tillbaka till huvudmenyn
+
+    Välj ett alternativ (1-5):
+  ''');
+  while (!validParkingSpaceInput) {
+    final input = stdin.readLineSync();
+    switch (input) {
+      case '1':
+        validateParkingSpaceInput();
+        print('nu får du skapa en parkeringsplats!');
+        break;
+      case '2':
+        validateParkingSpaceInput();
+        print('nu visas alla parkeringsplatser!');
+        break;
+      case '3':
+        validateParkingSpaceInput();
+        print('nu får du uppdatera en parkeringsplats!');
+        break;
+      case '4':
+        validateParkingSpaceInput();
+        print('nu får du ta bort en parkeringsplats!');
+        break;
+      case '5':
+        validateParkingSpaceInput();
+        return;
+      default:
+        stdout.write('''
+
+          'Ogiltigt val. Välj gärna mellan alternativ 1-5'
+        ''');
+        break;
+    }
+  }
+}
+
+void parkingMainHandler() {
+  stdout.write('''
+
+    Du har valt att hantera parkeringar. Vad vill du göra?
+    1. Lägg till ny parkering
+    2. Visa alla parkeringar
+    3. Uppdatera parkering
+    4. Ta bort parkering
+    5. Gå tillbaka till huvudmenyn
+
+    Välj ett alternativ (1-5):
+  ''');
+  while (!validParkingInput) {
+    final input = stdin.readLineSync();
+    switch (input) {
+      case '1':
+        validateParkingInput();
+        print('nu får du skapa en parkering!');
+        break;
+      case '2':
+        validateParkingInput();
+        print('nu visas alla parkeringar!');
+        break;
+      case '3':
+        validateParkingInput();
+        print('nu får du uppdatera en parkering!');
+        break;
+      case '4':
+        validateParkingInput();
+        print('nu får du ta bort en parkering!');
+        break;
+      case '5':
+        validateParkingInput();
+        return;
+      default:
+        stdout.write('''
+
+          'Ogiltigt val. Välj gärna mellan alternativ 1-5'
+        ''');
         break;
     }
   }
@@ -67,9 +205,25 @@ void validatePersonInput() {
   validPersonInput = true;
 }
 
+bool validVehicleInput = false;
+void validateVehicleInput() {
+  validVehicleInput = true;
+}
+
+bool validParkingSpaceInput = false;
+void validateParkingSpaceInput() {
+  validParkingSpaceInput = true;
+}
+
+bool validParkingInput = false;
+void validateParkingInput() {
+  validParkingInput = true;
+}
+
 void main(List<String> arguments) {
   while (true) {
-    print('''
+    stdout.write('''
+
     Välkommen till Parkeringsappen!
     Vad vill du hantera?
     1. Personer
@@ -83,7 +237,10 @@ void main(List<String> arguments) {
 
     String? input = stdin.readLineSync();
     if (input == '5') {
-      print('Du har valt att avsluta, hej då!');
+      stdout.write('''
+
+        'Du har valt att avsluta, hej då!'
+      ''');
       break; // Exit the program
     }
     choiceHandler(input);
