642bc2a1d98a72a1a1509765505c5c45f1fb8f41
Kaosma <erre360@hotmail.com>
2025-01-29 21:32:44 +0100

    style(parking admin title): change the admin title to Swedish

--- Uppgift 4/parking_admin/lib/app/main.dart
+++ Uppgift 4/parking_admin/lib/app/main.dart
@@ -19,7 +19,7 @@ class ParkingAdminApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Parking Admin',
+        title: 'Parkering Admin',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
